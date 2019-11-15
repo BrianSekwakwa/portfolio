82c94e7f83494d336d393c92c552af11367a4812
Brian Sekwakwa <briansenpai14@gmail.com>
2019-11-15 15:21:33 +0200

    added the the pictures for the projects section

--- src/components/AboutProject.js
+++ src/components/AboutProject.js
@@ -21,18 +21,12 @@ function AboutProject(props) {
       <div className="about-project">
         <div className="about-project__image">images goes here</div>
         <div className="about-project__details">
-          <h1>Title</h1>
-          <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque nam
-            vero quis ratione accusamus aperiam iure neque, minus nihil officia?
-            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Aliquid
-            quis voluptas ullam accusantium assumenda quae, ducimus mollitia
-            sequi? Odit, magnam!
-          </p>
-          <p>HTML,CSS,JavaScript</p>
+          <h1>{title}</h1>
+          <p>{summary}</p>
+          <p>{tech_used}</p>
           <div className="about-project__details__links">
-            <a href="#">View Project</a>
-            <a href="#">View Repo</a>
+            <a href={project_link}>View Project</a>
+            <a href={project_repo}>View Repo</a>
           </div>
         </div>
       </div>
--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,16 +1,28 @@
 import React from "react";
 import projects_data from "../json_data/projects_data.json";
 import clearview from "../images/clearview.png";
+import mans from "../images/mans.png";
+import pixabay from "../images/pixabay.png";
+import recipe from "../images/recipe.png";
+import safari from "../images/safari.png";
+import weather from "../images/weather.png";
+import portfolio from "../images/portfolio.png";
 import { Link } from "react-router-dom";
 
 function Projects() {
   const { data } = projects_data;
   const imageObject = {
-    clearview: clearview
+    clearview: clearview,
+    mans: mans,
+    pixabay: pixabay,
+    recipe: recipe,
+    safari: safari,
+    weather: weather,
+    portfolio: portfolio
   };
   const cards = data.map(item => {
     return (
-      <Link 
+      <Link
         to={{
           pathname: "/AboutProject",
           state: {
@@ -21,12 +33,12 @@ function Projects() {
             project_repo: item.project_repo,
             project_link: item.project_link
           }
-        }} 
+        }}
         key={item.id}
       >
         <div className="projects__card-group__card-item">
           <span>{item.title}</span>
-          <img src={imageObject[item.image_name]} alt="project photo" />
+          <img src={imageObject[item.image_name]} alt="project" />
         </div>
       </Link>
     );
